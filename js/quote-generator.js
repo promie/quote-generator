f6be54e9e7af9c85863e5bc4d6ac3bfe444f9e3b
Promie Yutasane <promie.yutasane@gmail.com>
2017-10-29 09:45:25 +1100

    Updating app

--- js/quote-generator.js
+++ js/quote-generator.js
@@ -1,10 +1,37 @@
 //MODEL
 
+//Get an API
+
+function getInformation(){
+
+    var getAPI = new XMLHttpRequest();
+    getAPI.open('GET', "http://quotes.stormconsultancy.co.uk/random.json");
+    getAPI.onload = function() {
+    
+        var apiData = JSON.parse(getAPI.responseText);
+        
+        const author = document.getElementById('author');
+        author.innerHTML = apiData.author;
+    
+        const quote = document.getElementById('quote');
+        quote.innerHTML = apiData.quote;
+    
+    
+    }
+    
+    getAPI.send();
+
+}
 
 
-//CONTROLLER
+function getTwitter() {
+    alert('Under Construction');
+}
 
 
 
 //VIEW
+
+//Initialization
+getInformation();
 
