5fe35648576a54899b154f4d24fe66273206b7b3
Promie Yutasane <promie.yutasane@gmail.com>
2017-10-29 11:06:54 +1100

    Update

--- js/quote-generator.js
+++ js/quote-generator.js
@@ -5,7 +5,7 @@
 function getInformation(){
 
     var getAPI = new XMLHttpRequest();
-    getAPI.open('GET', "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?"");
+    getAPI.open('GET', "http://quotes.stormconsultancy.co.uk/random.json");
     getAPI.onload = function() {
     
         var apiData = JSON.parse(getAPI.responseText);
