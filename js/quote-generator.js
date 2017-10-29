a35891ada358976dd3b17519a96eb985690c6560
Promie Yutasane <promie.yutasane@gmail.com>
2017-10-29 20:53:04 +1100

    Updating Twitter Button

--- js/quote-generator.js
+++ js/quote-generator.js
@@ -1,38 +1,45 @@
 
 $(document).ready(function(){
 
-    init();
+    setInterval(init(), 10000);
 
     function init() {
 
         const URL = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?';
         
             $.getJSON(URL, data => {
+
+                var quote = data.quoteText;
+                $('#quote').html(formatQuote(quote));
                 
-                $('#quote').html(data.quoteText);
-                if(data.quoteText === ''){
+                var author = data.quoteAuthor;
+                if(author === ''){
                     
                     $('#author').html('Anonymous');
                 }else{
-                    $('#author').html(data.quoteAuthor);
+                    $('#author').html(author);
                 }
+               
+                var twitterURL = "https://twitter.com/intent/tweet?text=" + quote + " - " + author;
+                $("#twitter").on('click', function(){
+                    window.open(twitterURL, '_blank');
+                });
+            })}
 
+    $('#newQuote').on('click', init);
 
-            });}
-
-    $('#newQuote').click(function(){
-        init();
-    });
-
-    $('#newQuote2').click(function(){
-
-    // https://twitter.com/intent/tweet?hashtags=quotes,freecodecamp&related=freecodecamp&text=
-        alert('Under Construction!');
-    });
-
+//VIEW
+    
+    function formatQuote(quote){
+        return `<em>${quote}</em> - `;
+    }
 
 });
 
 
+/*
 
+- Github Logo
+- Refresh Button
 
+*/
